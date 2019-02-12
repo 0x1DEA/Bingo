fd59ab847589bdf2028299ca9657fbe00b078514
BuildTools <unconfigured@null.spigotmc.org>
2019-02-11 18:54:36 -0600

    Added authentication

--- commands/permcheck.js
+++ commands/permcheck.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, [permission]) => {
   if (!message.member.roles.has(adminRole.id))
-    return message.reply("Permission Denied.");
+    return message.reply("Permission Denied. You cannot use this command.");
   if(message.guild.members.find("id", client.user.id).hasPermission(permission)) {
     message.reply('Permission granted');
   } else {
