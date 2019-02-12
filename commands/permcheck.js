8d36c01203b38a14d927d2e16a6ea30a8f1596e5
BuildTools <unconfigured@null.spigotmc.org>
2019-02-11 18:53:21 -0600

    New command !permcheck <discord permission>

---
+++ commands/permcheck.js
@@ -0,0 +1,9 @@
+exports.run = (client, message, [permission]) => {
+  if (!message.member.roles.has(adminRole.id))
+    return message.reply("Permission Denied.");
+  if(message.guild.members.find("id", client.user.id).hasPermission(permission)) {
+    message.reply('Permission granted');
+  } else {
+    message.reply('No permission');
+  }
+};
